b5615eaa64b03ccfbb32d35f0ca4c50847882ba7
Jinxing Lin <172601673@qq.com>
2020-12-26 00:35:32 +0800

    update custom fn

--- backend/custom/bucket-notification.ts
+++ backend/custom/bucket-notification.ts
@@ -1,24 +1,42 @@
+import { CloudFormationCustomResourceEvent, Context } from "aws-lambda";
+import cfn from "cfn-response";
 import { CaptureAWS } from "../xray";
 
 const s3client = new CaptureAWS.S3();
 
-export function handler(event: {
-  ResourceProperties: { bucket: string; fnArn: string };
-  RequestType: "Create" | "Delete" | "Update";
-}) {
-  const {
-    ResourceProperties: { bucket, fnArn },
-    RequestType,
-  } = event;
-  if (RequestType === "Create") {
-    s3client.putBucketNotificationConfiguration({
-      Bucket: bucket,
-      NotificationConfiguration: {
-        LambdaFunctionConfigurations: [
-          { LambdaFunctionArn: fnArn, Events: ["s3:ObjectCreated:*"] },
-        ],
+export function handler(
+  ev: {
+    ResourceProperties: { bucket: string; fnArn: string };
+  } & CloudFormationCustomResourceEvent,
+
+  ctx: Context
+) {
+  const { bucket, fnArn } = ev.ResourceProperties;
+
+  const PRId = ev.RequestType !== "Create" ? ev.PhysicalResourceId : undefined;
+
+  if (ev.RequestType === "Create" || ev.RequestType === "Update") {
+    s3client.putBucketNotificationConfiguration(
+      {
+        Bucket: bucket,
+        NotificationConfiguration: {
+          LambdaFunctionConfigurations: [
+            { LambdaFunctionArn: fnArn, Events: ["s3:ObjectCreated:*"] },
+          ],
+        },
       },
-    });
+      (err, data) => {
+        if (err) {
+          console.error(cfn.FAILED, err);
+          return cfn.send(ev, ctx, cfn.FAILED, err, PRId);
+        }
+
+        console.log(cfn.SUCCESS, data);
+        return cfn.send(ev, ctx, cfn.SUCCESS, data, PRId);
+      }
+    );
+  } else {
+    cfn.send(ev, ctx, cfn.SUCCESS, {}, PRId);
   }
-  return "SUCCESS";
+  return ev.RequestType;
 }
