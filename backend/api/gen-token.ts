972d4945b5f553a137aff40c95184d02ec086b36
Jinxing Lin <172601673@qq.com>
2020-12-27 18:37:00 +0800

    update template

--- backend/api/gen-token.ts
+++ backend/api/gen-token.ts
@@ -1,3 +1,29 @@
+import { resp } from "../utils/resp";
+import { CaptureAWS } from "../xray";
+
+const ASSUME_ROLE_ARN: string = process.env.ASSUME_ROLE_ARN!;
+
 export async function handler() {
-  return { msg: "hello token api" };
+  const stsClient = new CaptureAWS.STS();
+
+  try {
+    const result: AWS.STS.AssumeRoleResponse = await new Promise(
+      (resolve, reject) => {
+        stsClient.assumeRole(
+          {
+            RoleArn: ASSUME_ROLE_ARN,
+            RoleSessionName: Date.now().toString(16),
+          },
+          (err, data) => {
+            if (err) return reject(err);
+            resolve(data);
+          }
+        );
+      }
+    );
+    return resp(result.Credentials, 200);
+  } catch (err) {
+    console.error(err);
+    return resp(null, 502);
+  }
 }
